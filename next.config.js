5e44a032f6f54699cc8028049c7a4670bb7420f3
uh_max <maksims.kadobnovs@gmail.com>
2023-07-03 17:55:10 +0100

    something for now

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    redirects: async () => {
+        return [
+            {
+                source: "/github",
+                destination: "https://github.com/m4ksims",
+                permanent: true
+            },
+
+            {
+                source: "/twitter",
+                destination: "https://twitter.com/m4ksims",
+                permanent: true
+            },
+        ]
+    }
+}
 
 module.exports = nextConfig
