4bb0cb06edd5530ca0343078fa34fbece0b9c372
uh_max <maksims.kadobnovs@gmail.com>
2023-07-17 20:43:13 +0100

    clean little design

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,24 @@
 import Link from "next/link";
-import external_link from "./external-link.svg"
+import Image from "next/image";
 
 export default function Home() {
-  const calculateAge = () => {
-    const birthday = new Date(2009, 2, 30); // March is represented by index 2
-    const today = new Date();
-    let age = today.getFullYear() - birthday.getFullYear();
-    const monthDiff = today.getMonth() - birthday.getMonth();
-    
-    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthday.getDate())) {
-      age--;
-    }
-    
-    return age;
-  };
-
   return (
-    <main className="p-4 sm:p-6 md:p-8">
-      <h1 className="font-bold text-4xl sm:text-5xl md:text-4xl text-center">hey, i'm maksims.</h1>
-      <p className="mt-2 sm:mt-4 md:mt-4 text-[#b6b6b6] max-w-2xl mx-auto">
-        I'm a {calculateAge()} year old backend engineer based in the United Kingdom. I try to create solutions to real-world problems using a number of programming languages and
-        frameworks including
-        <a href="https://www.python.org/" className="text-blue-500 hover:text-blue-700" style={{ textDecoration: 'none' }} target="_blank" rel="noopener noreferrer"> Python</a>,
-        <a href="https://golang.org/" className="text-blue-500 hover:text-blue-700" style={{ textDecoration: 'none' }} target="_blank" rel="noopener noreferrer"> Go</a>,
-        <a href="https://www.typescriptlang.org/" className="text-blue-500 hover:text-blue-700" style={{ textDecoration: 'none' }} target="_blank" rel="noopener noreferrer"> TypeScript</a>,
-        and libraries like
-        <a href="https://www.djangoproject.com/" className="text-blue-500 hover:text-blue-700" style={{ textDecoration: 'none' }} target="_blank" rel="noopener noreferrer"> Django</a>,
-        <a href="https://gofiber.io/" className="text-blue-500 hover:text-blue-700" style={{ textDecoration: 'none' }} target="_blank" rel="noopener noreferrer"> Fiber</a>,
-        <a href="https://nextjs.org/" className="text-blue-500 hover:text-blue-700" style={{ textDecoration: 'none' }} target="_blank" rel="noopener noreferrer"> Next.js</a>.
-      </p>
-
-      <div className="mt-4 mr-2 text-[#b6b6b6]">
-        <h3>
-          <Link className="mr-4" href="/github">Github</Link>
-          <Link href="/twitter">Twitter</Link>
-        </h3>
+    <main className="flex flex-col items-center">
+      <div>
+        <div>
+          <h1 className="font-bold text-4xl sm:text-5xl">
+            Hey, I'm <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#56CCF2] to-[#2F80ED]">Maksims</span>.
+          </h1>
+          <h2 className="text-2xl sm:text-2xl mt-2 sm:mt-3">Backend Developer</h2>
+        </div>
+        <div className="mt-5 sm:mt-6 flex">
+          <Link href="https://github.com/m2ksims" className="mr-3">
+            <Image src="/github.svg" width={30} height={30} alt="Github" />
+          </Link>
+          <Link href="https://twitter.com/m2ksims">
+            <Image src="/twitter.svg" width={30} height={30} alt="Twitter" />
+          </Link>
+        </div>
       </div>
     </main>
   );
