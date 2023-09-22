61afc45d82730f513b3a083031f529b9de48f142
Maksims K <maksims.kadobnovs@gmail.com>
2023-09-22 19:07:15 +0100

    change stuff cus im cool

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,11 @@ export default function Home() {
       <div>
         <div>
           <h1 className="text-4xl sm:text-5xl font-bold">
-            Hey, I'm <span className="text-blue-400">Maksims</span>.
+            Hey, I'm <span className="bg-gradient-to-r from-[#8686e0] to-[#348AC7] bg-clip-text text-transparent">Maksims</span>.
           </h1>
-          <h2 className="text-2xl sm:text-2xl mt-2 sm:mt-3">Backend Developer</h2>
+          <h2 className="text-2xl text-[#bebebe] sm:text-2xl mt-2 sm:mt-3">Backend Developer</h2>
         </div>
-        <div className="mt-5 sm:mt-6 flex">
+        <div className="mt-5 sm:mt-4 flex">
           <Link href="/github" className="mr-3">
             <Image src="/github.svg" width={30} height={30} alt="Github" />
           </Link>
