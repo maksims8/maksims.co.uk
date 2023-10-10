d85b535ce051b7f8cae99464d8508693294f87ba
Maksims K <maksims.kadobnovs@gmail.com>
2023-10-10 18:09:43 +0100

    change sme tings

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center">
+    <main className="flex flex-col items-center bg-slate-900">
       <div>
         <div>
-          <h1 className="text-4xl sm:text-5xl font-bold">
-            Hey, I'm <span className="bg-gradient-to-r from-[#8686e0] to-[#348AC7] bg-clip-text text-transparent">Maksims</span>.
+          <h1 className="text-4xl sm:text-7xl font-bold bg-gradient-to-r from-[#7ab0ee] to-[#1588da] bg-clip-text text-transparent">
+            maksims
           </h1>
-          <h2 className="text-2xl text-[#bebebe] sm:text-2xl mt-2 sm:mt-3">Backend Developer</h2>
+          <h2 className="text-2xl text-[#c5c5c5] sm:text-2xl mt-2">
+            Backend Developer
+          </h2>
         </div>
         <div className="mt-5 sm:mt-4 flex">
           <Link href="/github" className="mr-3">
